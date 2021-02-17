292e23fa00e42b82a60e5a826992b4463c633a35
Steelnox <eric14pastor@gmail.com>
2021-02-17 08:56:44 +0100

    Ejercicio 3 hecho

---
+++ UD31_Ej3/js/codigo.js
@@ -0,0 +1,8 @@
+var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+function mostrar_meses()
+{
+    for(var i=0; i<meses.length;i++)
+    {
+        document.write(meses[i] + "<br>");
+    }
+}
