fd0956d975af25d4f3250c129b6b9c795da7456c
Steelnox <eric14pastor@gmail.com>
2021-02-17 07:06:16 +0100

    Ejercicio 1 hecho

---
+++ UD31_Ej1/js/codigo.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo!"); //muestra por consola la frase hola mundo
+
+console.log("Soy el primer script"); //muestra por consola la frase soy el primer script
+
+alert("Activar JavaScript si no lo teneis activado") //muestra ventana que avisa que se active JavaScript
