523688a961f0c9b090451aee4a8118aafb676364
Steelnox <eric14pastor@gmail.com>
2021-02-17 07:14:30 +0100

    Ejercicio 2 hecho

---
+++ UD31_Ej2/js/codigo.js
@@ -0,0 +1,7 @@
+console.log("Hola Mundo!"); //muestra por consola la frase hola mundo
+
+console.log("Soy el primer script"); //muestra por consola la frase soy el primer script
+
+var mensaje = "¡Hola Mundo!\n Qué facil es incluir 'comillas simples'\n y \"comillas dobles\"";
+
+alert(mensaje) //muestra ventana que avisa que se active JavaScript
