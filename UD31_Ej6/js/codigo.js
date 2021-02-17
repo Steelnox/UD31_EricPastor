ea14990a1660fbeb9f4119332ed4b26bb08bf386
Steelnox <eric14pastor@gmail.com>
2021-02-17 10:07:17 +0100

    Ejercicio 6 hecho

---
+++ UD31_Ej6/js/codigo.js
@@ -0,0 +1,7 @@
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E','T'];
+
+var DNI = 39927565;
+
+var resto = DNI % 23;
+
+console.log (letras[resto]);
